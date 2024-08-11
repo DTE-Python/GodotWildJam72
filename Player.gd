8027feb9cad06587f14b0f62ca69e4bdd63ccac9
Tuesday <tuesdayhands@gmail.com>
2024-08-11 13:30:13 +0100

    Added toggleable flashlight

--- Player.gd
+++ Player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 @export var camera:Camera3D
-
+@export var flashlight:SpotLight3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 @export_range(0,0.1) var SENSITIVITY = 0.05
@@ -26,6 +26,8 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("flashlight"):
+		flashlight.visible = !flashlight.visible
 	if Input.is_action_just_pressed("escape"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	# Get the input direction and handle the movement/deceleration.
